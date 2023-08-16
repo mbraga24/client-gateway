3ccfd7fed803bd9780baabf4ff0f10a0c9188087
mrdbrg <rdg.brg@gmail.com>
2023-08-15 20:51:33 -0500

    ready, set, go!

---
+++ src/main/java/com/pnc/SpringPncApplication.java
@@ -0,0 +1,13 @@
+package com.pnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnc/SpringPncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
