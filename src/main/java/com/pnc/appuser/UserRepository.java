dc7f8bf5a09af3ebc0197f8d00f0226e99d957a1
mrdbrg <rdg.brg@gmail.com>
2023-08-18 20:12:32 -0500

    set up JwtAuthenticationFilter

--- src/main/java/com/pnc/appuser/UserRepository.java
+++ src/main/java/com/pnc/appuser/UserRepository.java
@@ -2,9 +2,11 @@ package com.pnc.appuser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 //@Transactional
 //@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    Optional<AppUser> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
