28f74b9f7ae420376690741b9c1fb906f11f3ff6
mrdbrg <rdg.brg@gmail.com>
2023-08-19 17:15:53 -0500

    add ApplicationConfig class / userDetailsService() method

--- src/main/java/com/pnc/appuser/UserService.java
+++ src/main/java/com/pnc/appuser/UserService.java
@@ -15,5 +15,4 @@ public class UserService {
         return userDAO.selectAllAppUsers();
     }
 
-
 }
---
+++ src/main/java/com/pnc/config/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package com.pnc.config;
+
+import com.pnc.appuser.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@AllArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+
+}
