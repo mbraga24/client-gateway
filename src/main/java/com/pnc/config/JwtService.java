e575fd588aa7bfb12a2773283ca2cf33df6013d1
mrdbrg <rdg.brg@gmail.com>
2023-08-19 16:50:23 -0500

    set up generateToken(Map<String, Object>, UserDetails) and generateToken(UserDetails)

--- src/main/java/com/pnc/config/JwtService.java
+++ src/main/java/com/pnc/config/JwtService.java
@@ -2,12 +2,18 @@ package com.pnc.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
 import java.security.KeyStore;
+import java.security.Signature;
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -23,6 +29,21 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 300_000))
+                .signWith(SignatureAlgorithm.HS256, getSigningKey())
+                .compact();
     }
 
     /**
