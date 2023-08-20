336105e91ed84f4b9d04c95f53019f56e60ff014
mrdbrg <rdg.brg@gmail.com>
2023-08-20 12:19:48 -0500

    update names / encode passwords

--- src/main/java/com/pnc/SpringPncApplication.java
+++ src/main/java/com/pnc/SpringPncApplication.java
@@ -2,17 +2,22 @@ package com.pnc;
 
 import com.pnc.user.User;
 import com.pnc.user.UserRepository;
-import com.pnc.user.UserRole;
+import com.pnc.user.Role;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class SpringPncApplication {
+
+	private final PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext =
@@ -24,24 +29,25 @@ public class SpringPncApplication {
 	@Bean
 	CommandLineRunner runner(UserRepository userRepository) {
 		return args -> {
-			User marlon = new User("Kevin",
+			User kevin = new User("Kevin",
 					"Rice",
 					"kevinRice",
 					"kevin@email.com",
-					"password",
-					UserRole.ADMIN,
+					passwordEncoder.encode("password"),
+					Role.ADMIN,
 					false,
 					true);
+
 			User dave = new User("Dave",
 			"Bean",
 			"daveBean",
 			"dave@email.com",
-			"password",
-			UserRole.USER,
+			passwordEncoder.encode("password"),
+			Role.USER,
 			false,
 			true);
 
-			List<User> users = List.of(marlon, dave);
+			List<User> users = List.of(kevin, dave);
 			userRepository.saveAll(users);
 		};
 	}
