4cd037ac958edcf1239c97e231d73e5b31098799
mrdbrg <rdg.brg@gmail.com>
2023-08-20 12:17:41 -0500

    update HomeController

--- src/main/java/com/pnc/home/HomeController.java
+++ src/main/java/com/pnc/home/HomeController.java
@@ -1,4 +1,17 @@
 package com.pnc.home;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home-controller")
 public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> welcomeHome() {
+        return ResponseEntity.ok("Welcome to PNC");
+    }
 }
