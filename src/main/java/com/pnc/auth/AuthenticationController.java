12ef5be3ba26c1326a92e77ba3dba7d4f3d21ae4
mrdbrg <rdg.brg@gmail.com>
2023-08-21 19:06:32 -0500

    change endpoint names

--- src/main/java/com/pnc/auth/AuthenticationController.java
+++ src/main/java/com/pnc/auth/AuthenticationController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/authentication")
 public class AuthenticationController {
 
     private final AuthenticationService authService;
