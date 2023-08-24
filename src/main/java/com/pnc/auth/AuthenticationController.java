4ff951b2d9b94203b1f2556adb37c61e0e960fd9
mrdbrg <rdg.brg@gmail.com>
2023-08-23 21:47:21 -0500

    successfully retrieve ip address from client

--- src/main/java/com/pnc/auth/AuthenticationController.java
+++ src/main/java/com/pnc/auth/AuthenticationController.java
@@ -1,21 +1,26 @@
 package com.pnc.auth;
 
+import com.pnc.clientinfo.ClientInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/authentication")
 public class AuthenticationController {
 
     private final AuthenticationService authService;
+    private final ClientInfo clientInfo;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        log.info("clientInfo :: ============> {}", clientInfo.getClientIpAddress());
         return ResponseEntity.ok(authService.register(request));
     }
 
