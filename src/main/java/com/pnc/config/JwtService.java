0105dff7ccfe9e391ba88871e554b12d1fe270c2
mrdbrg <rdg.brg@gmail.com>
2023-08-28 21:09:53 -0500

    change back

--- src/main/java/com/pnc/config/JwtService.java
+++ src/main/java/com/pnc/config/JwtService.java
@@ -3,13 +3,11 @@ package com.pnc.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
-import java.security.KeyStore;
-import java.security.Signature;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Base64;
