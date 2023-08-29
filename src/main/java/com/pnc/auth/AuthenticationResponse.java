77916fbc77546b16bb79ff2339f1d42ad400b5ea
mrdbrg <rdg.brg@gmail.com>
2023-08-28 21:08:48 -0500

    add username, city, and welcome message

--- src/main/java/com/pnc/auth/AuthenticationResponse.java
+++ src/main/java/com/pnc/auth/AuthenticationResponse.java
@@ -10,7 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationResponse {
-
+//    -	When all validation is passed, return a random uuid and a welcome message
+//    with his username and his City Name (resolved using ip-geolocation api)
     private String token;
 
 }
