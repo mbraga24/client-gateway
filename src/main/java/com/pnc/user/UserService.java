6358d441033927d725626dff04d16c028964b3ea
mrdbrg <rdg.brg@gmail.com>
2023-09-01 19:08:44 -0500

    UserService clean up / testing user not found

--- src/main/java/com/pnc/user/UserService.java
+++ src/main/java/com/pnc/user/UserService.java
@@ -3,21 +3,23 @@ package com.pnc.user;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class UserService {
 
-    private final UserDAO userDAO;
+    private final UserRepository userRepository;
 
     public List<User> getAllUsers() {
-        return userDAO.getAllUsers();
+        return userRepository.findAll();
     }
 
-    public boolean isEmailTaken(String email) {
-        return userDAO.isEmailTaken(email);
-    }
-
+    // Method just to test UserNotFoundException when user is not found
+    //    public Optional<User> findUserTest(String email) {
+    //        return Optional.empty();
+    //    }
 
 }
